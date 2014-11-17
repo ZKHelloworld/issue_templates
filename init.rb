a3c0ebfcb37d8426525f765c20be8ae81a2160f3
Zheng Kenghong <zhengkenghong@tp-link.net>
2014-11-17 19:04:32 +0800

    Renew plugin information

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
-Redmine::Plugin.register :rit do
-  name 'Rit plugin'
+Redmine::Plugin.register :issue_templates do
+  name 'issue_templates plugin'
   author 'Author name'
-  description 'This is a plugin for Redmine'
+  description 'Redmine Issue Templates'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ZKHelloworld/issue_templates'
+  author_url 'https://github.com/ZKHelloworld'
 end
