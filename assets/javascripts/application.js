3c1312ab1e6a93ca9c6130ddf0884f48c6a3668c
Zheng Kenghong <zhengkenghong@gmail.com>
2014-11-26 23:41:59 +0800

    Add issue notes support

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,17 +5,25 @@ $(function($) {
     $('#template_insert_button').click(function() {
         var id = $('#templates_insert_select').val();
         var template = $('#template_content_' + id).text();
-        var origin = $('#issue_description').val();
+        var origin;
 
-        // add two line if origin is not empty
+        // description
+        origin = $('#issue_description').val();
         origin += origin ? '\n\n' : '';
-
-        // origin editor
         $('#issue_description').val(origin + template);
-        // ckeditor
         try {
             if (CKEDITOR.instances.issue_description) {
                 CKEDITOR.instances.issue_description.setData(origin + template);
+            }
+        } catch (e) {}
+
+        // note
+        origin = $('#issue_notes').val();
+        origin += origin ? '\n\n' : '';
+        $('#issue_notes').val(origin + template);
+        try {
+            if (CKEDITOR.instances.issue_notes) {
+                CKEDITOR.instances.issue_notes.setData(origin + template);
             }
         } catch (e) {}
     });
